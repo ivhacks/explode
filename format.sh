d812e33dc72a376ed50c05c7c2f80975603e93eb
Iv Robinson <aerobinsoniv@gmail.com>
2026-01-02 22:26:24 -0800

    red green blue cubes

---
+++ format.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+uvx ty check
+
+# Clean up imports and fix other issues
+python -m ruff check --select I --fix *.py
+python -m ruff check --fix *.py
+
+# Format code
+python -m ruff format *.py
