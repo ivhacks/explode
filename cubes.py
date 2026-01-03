6945dc5b59d6cf3dd156269ddf848cea4dfd668e
Iv Robinson <aerobinsoniv@gmail.com>
2026-01-02 22:41:22 -0800

    First animation function

--- cubes.py
+++ cubes.py
@@ -1,41 +1,24 @@
-import bpy  # type: ignore
+import bpy
+
+
+def animate_assemble(obj: bpy.types.Object, end_frame: int):
+    original_z = obj.location.z
+
+    bpy.context.scene.frame_set(end_frame - 60)
+    obj.location.z = original_z + 1.0
+    obj.keyframe_insert(data_path="location", frame=end_frame - 60)
+
+    bpy.context.scene.frame_set(end_frame)
+    obj.location.z = original_z
+    obj.keyframe_insert(data_path="location", frame=end_frame)
+
 
 cube0 = bpy.data.objects["Cube0"]
 cube1 = bpy.data.objects["Cube1"]
 cube2 = bpy.data.objects["Cube2"]
 
-red = (1.0, 0.0, 0.0, 1.0)
-green = (0.0, 1.0, 0.0, 1.0)
-blue = (0.0, 0.0, 1.0, 1.0)
-
-cube0.data.materials.append(bpy.data.materials.new(name="Red"))
-material = cube0.data.materials[0]
-material.use_nodes = True
-nodes = material.node_tree.nodes
-nodes.clear()
-bsdf = nodes.new(type="ShaderNodeBsdfPrincipled")
-bsdf.inputs["Base Color"].default_value = red
-output = nodes.new(type="ShaderNodeOutputMaterial")
-material.node_tree.links.new(bsdf.outputs["BSDF"], output.inputs["Surface"])
-
-cube1.data.materials.append(bpy.data.materials.new(name="Green"))
-material = cube1.data.materials[0]
-material.use_nodes = True
-nodes = material.node_tree.nodes
-nodes.clear()
-bsdf = nodes.new(type="ShaderNodeBsdfPrincipled")
-bsdf.inputs["Base Color"].default_value = green
-output = nodes.new(type="ShaderNodeOutputMaterial")
-material.node_tree.links.new(bsdf.outputs["BSDF"], output.inputs["Surface"])
-
-cube2.data.materials.append(bpy.data.materials.new(name="Blue"))
-material = cube2.data.materials[0]
-material.use_nodes = True
-nodes = material.node_tree.nodes
-nodes.clear()
-bsdf = nodes.new(type="ShaderNodeBsdfPrincipled")
-bsdf.inputs["Base Color"].default_value = blue
-output = nodes.new(type="ShaderNodeOutputMaterial")
-material.node_tree.links.new(bsdf.outputs["BSDF"], output.inputs["Surface"])
+animate_assemble(cube0, 100)
+animate_assemble(cube1, 140)
+animate_assemble(cube2, 180)
 
 bpy.ops.wm.save_as_mainfile(filepath=bpy.data.filepath)
