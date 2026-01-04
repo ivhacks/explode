c8beb02e9cf8da1d518842d62ad281a862c3c75c
Iv Robinson <aerobinsoniv@gmail.com>
2026-01-03 23:06:55 -0800

    aesthetic collisions

--- animatelib.py
+++ animatelib.py
@@ -49,3 +49,96 @@ def animate_assemble(
         obj.location.y = loc[1]
         obj.location.z = loc[2]
         obj.keyframe_insert(data_path="location", frame=frame)
+
+
+def collide(
+    obj_a: bpy.types.Object,
+    obj_b: bpy.types.Object,
+    mass_a: float,
+    mass_b: float,
+    collision_coords_a: tuple[float, float, float],
+    collision_coords_b: tuple[float, float, float],
+    start_frame: int,
+    collision_frame: int,
+    end_frame: int,
+    distance: tuple[float, float, float],
+):
+    start_coords_a = (
+        collision_coords_a[0] + distance[0],
+        collision_coords_a[1] + distance[1],
+        collision_coords_a[2] + distance[2],
+    )
+
+    # calculate velocity of A at collision (derivative of t^5 easing at t=1 is 5)
+    frames_before_collision = collision_frame - start_frame
+    distance_magnitude = (distance[0] ** 2 + distance[1] ** 2 + distance[2] ** 2) ** 0.5
+    velocity_a_at_collision = 5 * distance_magnitude / frames_before_collision
+
+    # momentum conservation: v_combined = (m_a * v_a) / (m_a + m_b)
+    velocity_combined = (mass_a * velocity_a_at_collision) / (mass_a + mass_b)
+
+    # direction of travel (normalized distance vector, inverted since A moves toward collision)
+    direction = (
+        -distance[0] / distance_magnitude,
+        -distance[1] / distance_magnitude,
+        -distance[2] / distance_magnitude,
+    )
+
+    # how far do they travel after collision while decelerating linearly to stop?
+    # linear decel: average velocity is half of initial, distance = v_avg * time
+    frames_after_collision = end_frame - collision_frame
+    post_collision_distance = velocity_combined * frames_after_collision / 2
+
+    end_coords_a = (
+        collision_coords_a[0] + direction[0] * post_collision_distance,
+        collision_coords_a[1] + direction[1] * post_collision_distance,
+        collision_coords_a[2] + direction[2] * post_collision_distance,
+    )
+    end_coords_b = (
+        collision_coords_b[0] + direction[0] * post_collision_distance,
+        collision_coords_b[1] + direction[1] * post_collision_distance,
+        collision_coords_b[2] + direction[2] * post_collision_distance,
+    )
+
+    # animate A: ease-in from start to collision
+    for frame in range(start_frame, collision_frame + 1):
+        loc = get_location(
+            start_coords_a, collision_coords_a, start_frame, frame, collision_frame
+        )
+        obj_a.location.x = loc[0]
+        obj_a.location.y = loc[1]
+        obj_a.location.z = loc[2]
+        obj_a.keyframe_insert(data_path="location", frame=frame)
+
+    # animate B: stationary until collision
+    obj_b.location.x = collision_coords_b[0]
+    obj_b.location.y = collision_coords_b[1]
+    obj_b.location.z = collision_coords_b[2]
+    obj_b.keyframe_insert(data_path="location", frame=start_frame)
+    obj_b.keyframe_insert(data_path="location", frame=collision_frame)
+
+    # animate both: linear deceleration from collision to end
+    for frame in range(collision_frame + 1, end_frame + 1):
+        t = (frame - collision_frame) / frames_after_collision
+        # linear decel: position follows t * (2 - t) curve (integral of linear velocity decay)
+        ease = t * (2 - t)
+
+        loc_a = (
+            collision_coords_a[0] + (end_coords_a[0] - collision_coords_a[0]) * ease,
+            collision_coords_a[1] + (end_coords_a[1] - collision_coords_a[1]) * ease,
+            collision_coords_a[2] + (end_coords_a[2] - collision_coords_a[2]) * ease,
+        )
+        obj_a.location.x = loc_a[0]
+        obj_a.location.y = loc_a[1]
+        obj_a.location.z = loc_a[2]
+        obj_a.keyframe_insert(data_path="location", frame=frame)
+
+        loc_b = (
+            collision_coords_b[0] + (end_coords_b[0] - collision_coords_b[0]) * ease,
+            collision_coords_b[1] + (end_coords_b[1] - collision_coords_b[1]) * ease,
+            collision_coords_b[2] + (end_coords_b[2] - collision_coords_b[2]) * ease,
+        )
+        obj_b.location.x = loc_b[0]
+        obj_b.location.y = loc_b[1]
+        obj_b.location.z = loc_b[2]
+        obj_b.keyframe_insert(data_path="location", frame=frame)
--- snapping.py
+++ snapping.py
@@ -5,11 +5,22 @@ sys.path.insert(0, str(Path(__file__).parent))
 
 import bpy
 
-from animatelib import animate_assemble
+from animatelib import collide
 
 cube0 = bpy.data.objects["Cube0"]
 cube1 = bpy.data.objects["Cube1"]
 
-animate_assemble(cube1, 240, 1, (0.0, 5.0, 0.0))
+collide(
+    obj_a=cube1,
+    obj_b=cube0,
+    mass_a=1.0,
+    mass_b=8.0,
+    collision_coords_a=(0.0, 1.5, 0.0),
+    collision_coords_b=(0.0, 0.0, 0.0),
+    start_frame=0,
+    collision_frame=240,
+    end_frame=480,
+    distance=(0.0, 5.0, 0.0),
+)
 
 bpy.ops.wm.save_as_mainfile(filepath=bpy.data.filepath)
